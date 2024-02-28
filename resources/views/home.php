1c2816c7c5badf3fee7fc05ae85430feaf93ff5b
Admin <admin@vinasupport.com>
2024-02-28 22:42:00 +0700

    finish bai view

--- resources/views/home.php
+++ resources/views/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hoc Laravel</title>
+    <title>Unicode Hoc Laravel</title>
 </head>
 
 <body>
